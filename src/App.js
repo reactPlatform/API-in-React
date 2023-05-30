4a6dc469506b4dc40874f2bc5cfe8f19380939d3
kavi <sumathi291098@gmail.com>
2023-05-30 12:35:57 +0530

    Loading the application

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import {useState} from 'react';
 function App() {
   const [movies,setMovies] = useState([]);
-  const fetchHandler = () => {
-    
-    fetch('https://swapi.dev/api/films').then(response => {
-      return response.json();
-    }).then(data => {
-      setMovies(data.results);
-    })
+  const [isLoading,setLoading] = useState(false);
+  async function fetchHandler() {
+    setLoading(true);
+    const response = await fetch('https://swapi.dev/api/films');
+    const data = await response.json();
+    setMovies(data.results);
+    setLoading(false);
     
   }
   return (
@@ -17,9 +17,16 @@ function App() {
       <button onClick={() => fetchHandler()}>Fetch Movies</button>
       <ul>
         {
-          movies.map(item => <li>{item.title}</li>)
+          !isLoading && movies.map(item => <li>{item.title}</li>)
         }
+        
       </ul>
+      {
+        isLoading  &&  <p>Loading....</p>
+      }
+      {
+        isLoading && movies.length == 0 && <p>Found no movies</p>
+      }
       
     </div>
   );
